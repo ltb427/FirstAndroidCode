d9a898a7ac80e42214118d5722c1d09c6185d69c
ltb427 <1123039786@qq.com>
2019-06-27 22:12:01 +0800

    ListView的简单使用

--- app/src/main/java/com/example/firstandroidcode/MainActivity.java
+++ app/src/main/java/com/example/firstandroidcode/MainActivity.java
@@ -3,14 +3,24 @@ package com.example.firstandroidcode;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity
 {
+    private String[] data = {"Apple", "Banana", "Orange", "Watermelon",
+    "Pear", "Grape", "Pineapple", "Strawberry", "Cherry", "Mangeo",
+    "Apple", "Banana", "Orange", "WaterMElon", "Pear", "Grape",
+    "Pineapple", "Strawberry", "Cherry", "Mango"};
 	public static final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, data);
+        ListView listView = (ListView)findViewById(R.id.list_view);
+        listView.setAdapter(arrayAdapter);
     }
 }
