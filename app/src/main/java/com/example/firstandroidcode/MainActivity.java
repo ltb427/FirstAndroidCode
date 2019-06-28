ca30b4a764a4eb3cc4bba1701037fbf957acf11e
ltb427 <1123039786@qq.com>
2019-06-28 20:28:23 +0800

    listview点击事件

--- app/src/main/java/com/example/firstandroidcode/MainActivity.java
+++ app/src/main/java/com/example/firstandroidcode/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.firstandroidcode;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,10 +26,20 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initFruit();
-        FruitAdapter fruitAdapter = new FruitAdapter(MainActivity.this, R.layout.fruit_item,
+        final FruitAdapter fruitAdapter = new FruitAdapter(MainActivity.this, R.layout.fruit_item,
                 fruitList);
         ListView listView = (ListView)findViewById(R.id.list_view);
         listView.setAdapter(fruitAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
+            {
+                Fruit fruit = fruitAdapter.getItem(i);
+                Toast.makeText(MainActivity.this, fruit.getmName(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
     private void initFruit()
     {
