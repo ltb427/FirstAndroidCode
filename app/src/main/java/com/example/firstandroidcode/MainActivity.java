fce5bc8d03186eb6aa6e0fb0c9377e0371aa8acf
ltb427 <1123039786@qq.com>
2019-06-29 11:18:17 +0800

    按钮的使用

--- app/src/main/java/com/example/firstandroidcode/MainActivity.java
+++ app/src/main/java/com/example/firstandroidcode/MainActivity.java
@@ -3,14 +3,32 @@ package com.example.firstandroidcode;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
 	public static final String TAG = MainActivity.class.getSimpleName();
+	Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.id_btn);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        switch (view.getId())
+        {
+            case R.id.id_btn:
+                Toast.makeText(MainActivity.this, "You click btn!",
+                        Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
