aa23c0f56090141b01e3ac23f350107a34f0deb4
ltb427 <1123039786@qq.com>
2019-06-29 11:26:16 +0800

    textview的使用

--- app/src/main/java/com/example/firstandroidcode/MainActivity.java
+++ app/src/main/java/com/example/firstandroidcode/MainActivity.java
@@ -5,19 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
 	public static final String TAG = MainActivity.class.getSimpleName();
 	Button btn;
+	TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn = findViewById(R.id.id_btn);
+        textView = findViewById(R.id.id_text);
         btn.setOnClickListener(this);
+        textView.setOnClickListener(this);
     }
 
     @Override
@@ -28,6 +32,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.id_btn:
                 Toast.makeText(MainActivity.this, "You click btn!",
                         Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.id_text:
+                TextView tv = (TextView)view;
+                Toast.makeText(MainActivity.this, tv.getText(),
+                        Toast.LENGTH_SHORT).show();
+                break;
+            default:
                 break;
         }
     }
