d1405f6847d7862174332137e10c7be63ef35a98
ltb427 <1123039786@qq.com>
2019-06-29 11:30:32 +0800

    edittext的使用

--- app/src/main/java/com/example/firstandroidcode/MainActivity.java
+++ app/src/main/java/com/example/firstandroidcode/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 	public static final String TAG = MainActivity.class.getSimpleName();
 	Button btn;
 	TextView textView;
+	EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -20,6 +22,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         btn = findViewById(R.id.id_btn);
         textView = findViewById(R.id.id_text);
+        editText = findViewById(R.id.edit_text);
+        editText.setOnClickListener(this);
         btn.setOnClickListener(this);
         textView.setOnClickListener(this);
     }
@@ -36,6 +40,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.id_text:
                 TextView tv = (TextView)view;
                 Toast.makeText(MainActivity.this, tv.getText(),
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.edit_text:
+                Toast.makeText(MainActivity.this, editText.getText().toString(),
                         Toast.LENGTH_SHORT).show();
                 break;
             default:
