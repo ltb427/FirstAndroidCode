3cd560c9c813ab236a85a4c8a8a70ba8b0a8d037
ltb427 <1123039786@qq.com>
2019-07-02 21:49:54 +0800

    动态权限管理： 	1.在androidmanifest.xml中注册权限 	2.在代码中申请权限数组 	3.check权限（具体见代码）

--- app/src/main/java/com/example/firstandroidcode/MainActivity.java
+++ app/src/main/java/com/example/firstandroidcode/MainActivity.java
@@ -1,16 +1,75 @@
 package com.example.firstandroidcode;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
 	public static final String TAG = MainActivity.class.getSimpleName();
+	//回复码
+    private static final int PERMISSION_REQUEST_CODE = 10000;
+    //权限组
+	private String[] mPermissions = new String[]
+            {
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_PHONE_STATE
+            };
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkPermission();
     }
+    //检查权限
+    private void checkPermission()
+    {
+        boolean isAllGranted = checkPermissionAllGranted(mPermissions);
+        if (isAllGranted)
+        {
+            Toast.makeText(this, "Yes", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            ActivityCompat.requestPermissions(this, mPermissions, PERMISSION_REQUEST_CODE);
+        }
+    }
+    //判断权限
+    private boolean checkPermissionAllGranted(String[] permissions)
+    {
+        for (String permission : permissions)
+        {
+            if (ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    //回复权限
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+        {
+            //用户同意，执行操作
+            Toast.makeText(this, "Yes", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            finish();
+        }
+    }
+
 }
