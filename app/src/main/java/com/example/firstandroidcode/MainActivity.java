24b0ba16fc30c1dafa55feeaab92fecdd6bf2417
ltb427 <1123039786@qq.com>
2019-07-12 21:31:31 +0800

    网络监听广播： 	1.一个intentFilter实例，添加一个广播变化的动作 	2.一个继承自broadcastReceiver的内部类 	3.使用registerReceiver()注册广播 	4.在ondestroy中使用unregisterReceiver解绑注册

--- app/src/main/java/com/example/firstandroidcode/MainActivity.java
+++ app/src/main/java/com/example/firstandroidcode/MainActivity.java
@@ -1,16 +1,50 @@
 package com.example.firstandroidcode;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity
 {
+    private IntentFilter intentFilter;
+    private NetWorkChangeReceiver netWorkChangeReceiver;
 	public static final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        netWorkChangeReceiver = new NetWorkChangeReceiver();
+        //注册
+        registerReceiver(netWorkChangeReceiver, intentFilter);
     }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        unregisterReceiver(netWorkChangeReceiver);
+    }
+
+    class NetWorkChangeReceiver extends BroadcastReceiver
+    {
+        @Override
+        public void onReceive(Context context, Intent intent)
+        {
+            Toast.makeText(MainActivity.this, "network changes", Toast.LENGTH_SHORT)
+                    .show();
+        }
+    }
+
 }
