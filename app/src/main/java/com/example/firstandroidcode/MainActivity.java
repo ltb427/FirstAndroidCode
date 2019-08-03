986555a869989a5db19339cf9ed6f558ca646dea
ltb427 <1123039786@qq.com>
2019-08-03 13:13:06 +0800

    获取SharedPreferences的三种方法 		1.使用Context类中的getShredPreferences() 		2.Activity类中的getPreferences() 		3.PreferenceManager类中getDefaultSharedPreferences()方法 使用方法分为三步 		1.SharedPreferences.Edior对象 		2.向edior中添加数据 		3.调用apply()对象保存数据 		4.读取使用getSharedPreferences(name,mode)读取数据

--- app/src/main/java/com/example/firstandroidcode/MainActivity.java
+++ app/src/main/java/com/example/firstandroidcode/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.firstandroidcode;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -12,5 +16,33 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button save_button = findViewById(R.id.save_button);
+        save_button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                SharedPreferences.Editor editor = getSharedPreferences("data", MODE_PRIVATE).edit();
+                editor.putString("name", "Tom");
+                editor.putInt("age", 28);
+                editor.putBoolean("married", false);
+                editor.apply();
+            }
+        });
+        Button read_button = findViewById(R.id.read_button);
+        read_button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                SharedPreferences pref = getSharedPreferences("data", MODE_PRIVATE);
+                String name = pref.getString("name", "");
+                int age = pref.getInt("age", 0);
+                boolean married = pref.getBoolean("married", false);
+                Log.d(TAG, "name is " + name);
+                Log.d(TAG, "age is " + age);
+                Log.d(TAG, "married is " + married);
+            }
+        });
     }
 }
