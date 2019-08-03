49e144389e475472ec10da3259e6cf6036a3e2b5
ltb427 <1123039786@qq.com>
2019-08-03 19:34:52 +0800

    添加数据

--- app/src/main/java/com/example/firstandroidcode/MainActivity.java
+++ app/src/main/java/com/example/firstandroidcode/MainActivity.java
@@ -2,14 +2,19 @@ package com.example.firstandroidcode;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
 	public static final String TAG = MainActivity.class.getSimpleName();
 	private MyDatabaseHelper myDatabaseHelper;
+	private Button create_btn;
+	private Button add_data_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -17,14 +22,43 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         myDatabaseHelper = new MyDatabaseHelper(MainActivity.this,"BookStore.db",
                 null,2);
-        Button create_btn = findViewById(R.id.create_book);
-        create_btn.setOnClickListener(new View.OnClickListener()
+        create_btn = findViewById(R.id.create_table);
+        create_btn.setOnClickListener(this);
+        add_data_btn = findViewById(R.id.add_data);
+        add_data_btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        switch (view.getId())
         {
-            @Override
-            public void onClick(View view)
-            {
+            case R.id.create_table:
                 myDatabaseHelper.getWritableDatabase();
-            }
-        });
+                break;
+            case R.id.add_data:
+                SQLiteDatabase db = myDatabaseHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                //组装第一条数据
+                values.put("name", "The Da Vinci Code");
+                values.put("author", "Dan Brown");
+                values.put("pages", 454);
+                values.put("price", 16.96);
+                //插入第一条数据
+                db.insert("book", null , values);
+                values.clear();
+                //组装第二条数据
+                values.put("name", "Tho Lost Symbol");
+                values.put("author", "Dan Brown");
+                values.put("pages", 510);
+                values.put("price", 19.95);
+                //插入第二条数据;
+                db.insert("book", null, values);
+                values.clear();
+                Toast.makeText(this, "Add succeeded", Toast.LENGTH_SHORT).show();
+                break;
+                default:
+                    break;
+        }
     }
 }
