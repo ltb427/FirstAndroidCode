ab670b468bb3fce4933f81efb544055445af6080
ltb427 <1123039786@qq.com>
2019-09-20 10:08:41 +0800

    先保存，可能没用。

--- app/src/main/java/com/example/firstandroidcode/MainActivity.java
+++ app/src/main/java/com/example/firstandroidcode/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.firstandroidcode;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import org.litepal.LitePal;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -12,5 +16,14 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button createDatabase = findViewById(R.id.btn_create);
+        createDatabase.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                LitePal.getDatabase();
+            }
+        });
     }
 }
